192d2326d3fde3629d6ddca518d21beb3073603c
anaflv <ana-flavia@hotmail.co.uk>
2015-07-13 14:28:59 -0300

    Second green square
    Now there is a second green square in the swimming game, that will also
    tell the player when to do armstrokes

--- JecripePara/Assets/Swimming/Resources/Scripts/CountDown.cs
+++ JecripePara/Assets/Swimming/Resources/Scripts/CountDown.cs
@@ -7,7 +7,6 @@ public class CountDown : MonoBehaviour
 
 	
 	public float seconds = 3;
-	bool showCountdown;
 	bool countdown;
 	Timer Timer;
 	public string secString;
@@ -34,8 +33,8 @@ public class CountDown : MonoBehaviour
 	{
 		Timer.SetTimer();
 		seconds = 3;
-		showCountdown = true;
 		countdown = true;
+		waitButton.SetActive(true);
 		
 	}
 	
@@ -43,29 +42,13 @@ public class CountDown : MonoBehaviour
 		if(seconds > 0){
 			seconds -= Time.deltaTime;
 			secString = seconds.ToString("0");
-			waitButton.SetActive(true);
 			waitText.text = "AGUARDE\n " + secString;
 		} else if (seconds <= 0){
 			countdown = false;
-			showCountdown = false;
 			waitButton.SetActive(false);
 		}
 	}
 
-	/*
-	void OnGUI ()
-	{
-		
-		float width = 20;
-		float height = 50;
-		GUI.skin.label.fontSize = 26;
-		GUI.skin.label.normal.textColor = Color.black;
-		GUI.skin.label.hover.textColor = Color.black;
-		
-		if(showCountdown){
-			GUI.Label (new Rect (Screen.width * 1 / 2 - width * 1 / 2, Screen.height * 1 / 5 - height * 1 / 2, width, height), seconds.ToString ("0"));
-		}
-		
-	}*/
+
 	
 }
