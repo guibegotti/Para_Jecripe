1b1f4e643c22264988174f607098f1daa1d86aca
anaflv <ana-flavia@hotmail.co.uk>
2015-07-23 09:42:17 -0300

    Prefabs added to the Athletics game

---
+++ JecripePara/Assets/AthleticsController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AthleticsController : MonoBehaviour {
+
+
+	GameObject gameOverCanvas;
+	GameObject canvas;
+	public Text result;
+
+	
+	void Start () {
+		
+		gameOverCanvas = GameObject.Find ("GameOverCanvas");
+		canvas = GameObject.Find("Canvas");
+		
+		gameOverCanvas.SetActive(false);
+		
+	
+	}
+	
+	public void Reload(){
+		
+		Application.LoadLevel(Application.loadedLevel);
+	
+	}
+	
+	public void BackToMenu(){
+		Application.LoadLevel ("PlayAthletics");
+	}
+
+	
+	public void GameOver(string loser, string winner, float loserTime, float winnerTime){
+	
+		gameOverCanvas.SetActive(true);
+		canvas.SetActive(false);
+		
+		result.text = "RESULTADOS:\n\n1. " + winner + ": " + winnerTime.ToString("0.0") + "\n2. " + loser + ": " + loserTime.ToString("0.0");
+	}
+}
