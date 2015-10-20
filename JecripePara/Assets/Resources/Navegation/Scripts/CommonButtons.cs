9ff7cf8ac77c8837ad55f101a777fdf8ad011d1f
anaflv <ana-flavia@hotmail.co.uk>
2015-10-20 16:32:18 -0200

    .

--- JecripePara/Assets/Resources/Navegation/Scripts/CommonButtons.cs
+++ JecripePara/Assets/Resources/Navegation/Scripts/CommonButtons.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class CommonButtons : MonoBehaviour {
 
 	
 	public string scene;
+	public string text2String;
+	GameObject text2;
 	
 	public void QuitApp(){
 		
@@ -23,6 +26,24 @@ public class CommonButtons : MonoBehaviour {
 	public void LoadScene(){
 		
 		Application.LoadLevel(scene);
+	}
+	
+	
+	public void ReloadScene(){
+		
+		Application.LoadLevel(Application.loadedLevel);
+	}
+	
+	public void ShowText(string text){
+	
+		text2 = GameObject.Find("ChangingText");
+		text2.GetComponent<Text>().text = text2String;
+	}
+	
+	public void ClearText(){
+	
+		text2 = GameObject.Find("ChangingText");
+		text2.GetComponent<Text>().text = "";
 	}
 	
 	
