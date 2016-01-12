f10b25e7a1c0b576389a3e5bde7dda51edca5a1d
anaflv <ana-flavia@hotmail.co.uk>
2016-01-12 10:57:36 -0200

    Long Jump mini-game: Pause Canvas implemented

---
+++ JecripePara/Assets/Resources/Athletics/Scripts/LongJump_GameController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LongJump_GameController : MonoBehaviour {
+
+	GameObject pauseCanvas;
+	GameObject canvas1;
+	
+	void Start(){
+		
+		pauseCanvas = GameObject.Find ("PauseCanvas");
+		pauseCanvas.SetActive(false);
+		
+		canvas1 = GameObject.Find ("Canvas");
+	}
+	
+	void Update(){
+		
+		if(Input.GetKeyDown(KeyCode.P)){
+			
+			PauseGame();
+		}
+	}
+	
+	
+	public void PauseGame(){
+	
+		if(Time.timeScale == 1){
+			Time.timeScale = 0;
+			pauseCanvas.SetActive(true);
+			canvas1.SetActive(false);
+		} else if(Time.timeScale == 0){
+			Time.timeScale = 1;
+			pauseCanvas.SetActive(false);
+			canvas1.SetActive(true);
+		}
+	}
+	
+	
+	
+	
+}
