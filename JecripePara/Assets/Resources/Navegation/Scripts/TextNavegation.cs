ee3d0cb1d30f1e60efca3604291b1d172d87bc15
anaflv <ana-flavia@hotmail.co.uk>
2016-01-27 12:11:54 -0200

    Navegation through text pages: script created
    - Test scene created (“TEST SCENE”)
    - Navegtation system created: navegation through multiple pages of
    text. The previous arrow only appears on the second page and the next
    arrow disappears on the last page. All the gameobjects and strings on
    the script are public so as to allow the script to be used on any menu
    scene for any sport in the game.

---
+++ JecripePara/Assets/Resources/Navegation/Scripts/TextNavegation.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextNavegation : MonoBehaviour {
+
+	int count = 0;
+	public int max;
+	
+	public string text0;
+	public string text1;
+	public string text2;
+	public string text3;
+	
+	public Text text;
+	public GameObject nextArrow;
+	public GameObject previousArrow;
+	
+		
+	void Update(){
+	
+		if (count == 0){
+			
+			previousArrow.SetActive(false);
+			text.text = text0;
+			
+		} else if(count == 1){
+			
+			previousArrow.SetActive(true);
+			text.text = text1;
+			
+		} else if (count == 2){
+		
+			text.text = text2;
+			
+		} else if (count == 3){
+			
+			text.text = text3;
+		}
+		
+		if (count == max){
+			
+			nextArrow.SetActive(false);
+		} else {
+			
+			nextArrow.SetActive(true);
+		}
+		
+		
+	}
+
+	
+	
+	public void nextTextPage(){
+	
+		count++;
+	
+		
+	}
+	
+	public void previousTextPage(){
+		
+		count--;
+	}
+	
+	
+	
+	
+}
