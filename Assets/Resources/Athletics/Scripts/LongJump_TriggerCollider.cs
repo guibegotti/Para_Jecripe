45e990ba3ddef0244157343570a25db2625caa3c
Ana Flavia de Araujo <ana-flavia@hotmail.co.uk>
2016-05-10 17:25:19 -0300

    Long Jump: scripts realocated to the right place

---
+++ Assets/Resources/Athletics/Scripts/LongJump_TriggerCollider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LongJump_TriggerCollider : MonoBehaviour {
+
+
+	void OnTriggerEnter(Collider other){
+
+		LongJump_Tutorial.count = 4;
+	
+	}
+}
