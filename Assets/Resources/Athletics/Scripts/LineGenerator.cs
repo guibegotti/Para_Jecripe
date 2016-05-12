3efdc1719c8de7d69b1619c55ea0b2898cdbb285
alanAguiar <alan.aguiar@aluno.ufabc.edu.br>
2016-05-12 19:46:57 -0300

    Hand band update

---
+++ Assets/Resources/Athletics/Scripts/LineGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineGenerator : MonoBehaviour {
+
+    private LineRenderer l;
+    public Transform athlete1;
+    public Transform guide2;
+
+    // Use this for initialization
+    void Start () {
+        l = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        l.SetPosition(0, athlete1.position);
+        l.SetPosition(1, guide2.position);
+	}
+}
