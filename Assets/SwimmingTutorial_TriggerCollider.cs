fae1e3895314043e7ef29328855feb2551bee981
Ana Flavia de Araujo <ana-flavia@hotmail.co.uk>
2016-07-08 22:10:35 -0300

    Swimming tutorial: bug fixed (player cannot go through the pool wall)

---
+++ Assets/SwimmingTutorial_TriggerCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwimmingTutorial_TriggerCollider : MonoBehaviour {
+
+
+	void OnTriggerEnter(Collider other){
+
+
+		Application.LoadLevel ("Swimming");
+	}
+
+}
